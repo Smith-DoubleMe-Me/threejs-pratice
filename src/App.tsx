ce09c1de9c03959802b963f9e8437296b8a89a02
smith-doubleme-me <smith.jang@doubleme.me>
2023-07-14 18:02:58 +0900

    react-three/fiber practice 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { Canvas } from "@react-three/fiber"
+import { useRef, useState } from "react";
+import { Canvas, ThreeElements, useFrame } from "@react-three/fiber"
+
+function Box(props: ThreeElements["mesh"]) {
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState<boolean>(false);
+  const [active, setActive] = useState<boolean>(false);
+
+  useFrame((_, delta) => (meshRef.current.rotation.x += delta));
+
+  return (
+    <mesh {...props} ref={meshRef} scale={active ? 1.5 : 1} onClick={(prev) => setActive(!prev)} onPointerOver={() => setHover(true)} onPointerOut={() => setHover(false)}>
+      <boxGeometry args={[2,2,2]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  )
+}
 
 function App() {
+  
+
   return (
     <>
       <h1>Three.js practice</h1>
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <mesh position={[0,0,0]}>
-          <boxGeometry args={[1,1,1]} />
-          <meshStandardMaterial color={0x00ff00} />
-        </mesh>
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
       </Canvas>
     </>
   )
